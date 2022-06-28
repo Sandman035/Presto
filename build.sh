dcb76a91afaae3971bde369e9633fe55fb10eaf3
Sandman035 <sandor.skt@gmail.com>
2022-06-28 11:54:40 -0400

    Prepare project

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set echo on
+
+mkdir -p bin
+
+# Get a list of all the .cpp files.
+cFilenames=$(find . -type f -name "*.cpp")
+
+# echo "Files:" $cFilenames
+
+assembly="presto"
+compilerFlags="-g" 
+includeFlags="-Isrc"
+linkerFlags="-lstdc++"
+defines=""
+#defines="-DIMPORT"
+
+echo "Building $assembly..."
+echo clang $cFilenames $compilerFlags -o bin/$assembly $defines $includeFlags $linkerFlags
+clang $cFilenames $compilerFlags -o bin/$assembly $defines $includeFlags $linkerFlags
