5b9680ebaaff81c71daf8e5f0b7264d23e4ca836
Sandman035 <sandor.skt@gmail.com>
2022-08-24 18:45:37 -0400

    Fix bug that keeps empty workspaces active after unmaping it

--- src/workspaces.cpp
+++ src/workspaces.cpp
@@ -6,11 +6,13 @@
 namespace presto {
 	void changeWorkspace(WindowManager* wm, int number) {
 		int monitor = getMonitorUnderCursor(wm->connection, wm->screen->root, wm->monitors);
-		if (wm->monitors[monitor].currentWorkspace != number) {
+
+		if (wm->monitors[monitor].currentWorkspace != number) { //If the current workspace is not the wanted workspace
 			std::list<xcb_window_t>::iterator it;
-			if (wm->workspaces[number].active && wm->workspaces[number].monitor != monitor) {
-				if (wm->monitors[wm->workspaces[number].monitor].currentWorkspace != number) {
-					if (!wm->workspaces[wm->monitors[wm->workspaces[number].monitor].currentWorkspace].windows.empty()) {
+
+			if (wm->workspaces[number].active && wm->workspaces[number].monitor != monitor) { //If the workspace is active and the workspace is on a different monitor
+				if (wm->monitors[wm->workspaces[number].monitor].currentWorkspace != number) { //If the current workspace of the other monitor is not the wanted workspace
+					if (!wm->workspaces[wm->monitors[wm->workspaces[number].monitor].currentWorkspace].windows.empty()) { //If the current monitor of the other monitor is not empty
 						for (it = wm->workspaces[wm->monitors[wm->workspaces[number].monitor].currentWorkspace].windows.begin(); it != wm->workspaces[wm->monitors[wm->workspaces[number].monitor].currentWorkspace].windows.end(); ++it) {
 							xcb_unmap_window(wm->connection, *it);
 						}
@@ -18,20 +20,25 @@ namespace presto {
 						wm->workspaces[wm->monitors[wm->workspaces[number].monitor].currentWorkspace].active = false;
 					}
 				}
+
+				//Move cursor to the other monitor
 				xcb_warp_pointer(wm->connection, XCB_NONE, wm->screen->root, 0, 0, 0, 0, wm->monitors[wm->workspaces[number].monitor].width / 2 + wm->monitors[wm->workspaces[number].monitor].x, wm->monitors[wm->workspaces[number].monitor].height / 2 + wm->monitors[wm->workspaces[number].monitor].y);
 				xcb_flush(wm->connection);
-				wm->monitors[wm->workspaces[number].monitor].currentWorkspace = number;
-			} else {
+				wm->monitors[wm->workspaces[number].monitor].currentWorkspace = number; //make the current workspace of the monitor the wanted workspace
+
+			} else { //If the wanted workspace is not active or its on this monitor
+
+				//Unmap all windows of the current workspace
 				for (it = wm->workspaces[wm->monitors[monitor].currentWorkspace].windows.begin(); it != wm->workspaces[wm->monitors[monitor].currentWorkspace].windows.end(); ++it) {
 					xcb_unmap_window(wm->connection, *it);
 				}
 
-				wm->monitors[monitor].currentWorkspace = number;
-				wm->workspaces[number].monitor = monitor;
-				
 				if (wm->workspaces[wm->monitors[monitor].currentWorkspace].windows.empty()) {
 					wm->workspaces[wm->monitors[monitor].currentWorkspace].active = false;
 				}
+
+				wm->monitors[monitor].currentWorkspace = number;
+				wm->workspaces[number].monitor = monitor;
 			}
 
 			for (it = wm->workspaces[number].windows.begin(); it != wm->workspaces[number].windows.end(); ++it) {
